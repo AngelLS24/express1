bc9ac164054c805e38b2ac80f7f5c44ebb1025e0
AngelLS24 <angls2495@gmail.com>
2019-05-25 15:25:36 -0500

    commit con cambios en vistas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sistemas de Informacion' });
 });
+
+router.get('/apartados', function(req, res, next) {
+  res.render('apartados', { title: 'Apartados' });
+});
+
+router.get('/alumnos', function(req, res, next){
+	res.render('alumnos', {title: 'Alumnos' });
+});
+
+/* Imprime un mensaje en la pagina inicial */
+/*
+define the home page route
+router.get('/', function(req, res) {
+  res.send('Birds home page');
+});
+ define the about route
+router.get('/about', function(req, res) {
+  res.send('About birds');
+});*/
 
 module.exports = router;
